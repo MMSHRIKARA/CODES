919810c06d72accfbd00e04e5fccfd775c00d25d
KAIZER <shrikara21@gmail.com>
2024-05-31 14:57:55 +0530

    snakecase

--- Cases/src/com/SnakeCase.java
+++ Cases/src/com/SnakeCase.java
@@ -1,15 +1,16 @@
 package com;
 
-public class CamelCase {
+public class SnakeCase {
 	public static void main(String[] args) {
-		String str = "my name is Kaizer";
+		String str = "My Name is Kaizer";
 		System.out.println("I/P : "+str);
 		System.out.print("O/P : ");
 		char[] arr = str.toCharArray();
 		arr[0] = Character.toLowerCase(arr[0]);
 		for(int i=0; i<str.length(); i++) {
 			if(arr[i]==' ') {
-				arr[i+1]=Character.toUpperCase(arr[i+1]);
+				arr[i+1]=Character.toLowerCase(arr[i+1]);
+				System.out.print("_");
 			}else {
 				
 			System.out.print(arr[i]);
